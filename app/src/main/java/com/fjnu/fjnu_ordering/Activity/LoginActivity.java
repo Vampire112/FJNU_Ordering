e7c667f8265edb6a2a8728aafcc8887f1bed0769
vampire <34119550+Vampire112@users.noreply.github.com>
2018-03-19 21:54:58 +0800

    Initial commit

---
+++ app/src/main/java/com/fjnu/fjnu_ordering/Activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.fjnu.fjnu_ordering.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fjnu.fjnu_ordering.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
